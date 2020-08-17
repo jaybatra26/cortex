83b3c43fc142b36d38187fbc84c5ba459e1a95d0
Jay Batra <jaybatra73@gmail.com>
2020-08-17 14:42:50 +0530

    Update logic for exclude Labels

--- pkg/chunk/testutils/testutils.go
+++ pkg/chunk/testutils/testutils.go
@@ -136,7 +136,7 @@ func SetupTestChunkStore() (chunk.Store, error) {
 	var storeCfg chunk.StoreConfig
 	flagext.DefaultValues(&storeCfg)
 
-	store := chunk.NewCompositeStore(nil, nil)
+	store := chunk.NewCompositeStore(nil)
 	err = store.AddPeriod(storeCfg, schemaCfg.Configs[0], storage, storage, overrides, cache.NewNoopCache(), cache.NewNoopCache())
 	if err != nil {
 		return nil, err
