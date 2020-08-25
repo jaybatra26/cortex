2ac68d9fccc103a89ba00753c5de39d52d95c0fb
Jay Batra <jaybatra73@gmail.com>
2020-08-25 12:49:45 +0530

    Fixing build errors

--- pkg/chunk/chunk_store_test.go
+++ pkg/chunk/chunk_store_test.go
@@ -883,8 +883,8 @@ func (m mockSeriesStoreSchema) GetLabelNamesForSeries(from, through model.Time, 
 	return m.schema.GetLabelNamesForSeries(from, through, userID, seriesID)
 }
 
-func (m mockSeriesStoreSchema) GetSeriesDeleteEntries(from, through model.Time, userID string, metric labels.Labels, hasChunksForIntervalFunc hasChunksForIntervalFunc) ([]IndexEntry, error) {
-	return m.schema.GetSeriesDeleteEntries(from, through, userID, metric, hasChunksForIntervalFunc)
+func (m mockSeriesStoreSchema) GetSeriesDeleteEntries(from, through model.Time, userID string, metric labels.Labels, hasChunksForIntervalFunc hasChunksForIntervalFunc, excfg util.ExcludeLabels) ([]IndexEntry, error) {
+	return m.schema.GetSeriesDeleteEntries(from, through, userID, metric, hasChunksForIntervalFunc, excfg)
 }
 
 func mustNewLabelMatcher(matchType labels.MatchType, name string, value string) *labels.Matcher {
@@ -1434,7 +1434,7 @@ func TestStore_DeleteSeriesIDs(t *testing.T) {
 			require.Equal(t, 2, len(chunks))
 
 			// lets try deleting series ID without deleting the chunk
-			err = store.DeleteSeriesIDs(ctx, fooChunk1.From, fooChunk1.Through, userID, fooChunk1.Metric)
+			err = store.DeleteSeriesIDs(ctx, fooChunk1.From, fooChunk1.Through, userID, fooChunk1.Metric, excludeLblCfg)
 			require.NoError(t, err)
 
 			// series IDs should still be there since chunks for them still exist
@@ -1447,7 +1447,7 @@ func TestStore_DeleteSeriesIDs(t *testing.T) {
 			err = store.DeleteChunk(ctx, fooChunk1.From, fooChunk1.Through, userID, fooChunk1.ExternalKey(), metric1, nil)
 			require.NoError(t, err)
 
-			err = store.DeleteSeriesIDs(ctx, fooChunk1.From, fooChunk1.Through, userID, fooChunk1.Metric)
+			err = store.DeleteSeriesIDs(ctx, fooChunk1.From, fooChunk1.Through, userID, fooChunk1.Metric, excludeLblCfg)
 			require.NoError(t, err)
 
 			// there should be only be 1 chunk and 1 series ID left for it
@@ -1466,7 +1466,7 @@ func TestStore_DeleteSeriesIDs(t *testing.T) {
 				&model.Interval{Start: fooChunk2.From, End: fooChunk2.From.Add(30 * time.Minute)})
 			require.NoError(t, err)
 
-			err = store.DeleteSeriesIDs(ctx, fooChunk1.From, fooChunk1.Through, userID, fooChunk1.Metric)
+			err = store.DeleteSeriesIDs(ctx, fooChunk1.From, fooChunk1.Through, userID, fooChunk1.Metric, excludeLblCfg)
 			require.NoError(t, err)
 
 			// partial deletion should have left another chunk and a series ID in store
